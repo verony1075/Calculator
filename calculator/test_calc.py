f954ac3ef7e9111df71a336fbc49f6e4aa5a80d5
Veronica Agyapong <veronysuccess@gmail.com>
2024-06-26 05:19:21 +0000

    added the test_calc file

--- calculator/test_calc.py
+++ calculator/test_calc.py
@@ -8,9 +8,31 @@ class TestCalc(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-2, 3), 1)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_sub(self):
         self.assertEqual(self.calc.sub(2, 3), -1)
+        self.assertEqual(self.calc.sub(-2, 3), -5)
+        self.assertEqual(self.calc.sub(0, 0), 0)
 
     def test_mul(self):
         self.assertEqual(self.calc.mul(2, 3), 6)
+        self.assertEqual(self.calc.mul(-2, 3), -6)
+        self.assertEqual(self.calc.mul(0, 5), 0)
+
+    def test_div(self):
+        self.assertEqual(self.calc.div(6, 3), 2)
+        self.assertEqual(self.calc.div(-6, 3), -2)
+        self.assertEqual(self.calc.div(0, 5), 0)
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.div(5, 0)
+
+    def test_pow(self):
+        self.assertEqual(self.calc.pow(2, 3), 8)
+        self.assertEqual(self.calc.pow(-2, 3), -8)
+        self.assertEqual(self.calc.pow(0, 5), 0)
+        self.assertEqual(self.calc.pow(1, 100), 1)
+
+if __name__ == '__main__':
+    unittest.main()
